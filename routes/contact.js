5da7c368b5ab34cb83f875ed9c18c15cb0f233d6
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-23 17:56:48 +0530

    Add files via upload

---
+++ routes/contact.js
@@ -0,0 +1,12 @@
+const path=require('path');
+const express=require('express');
+
+const routeDir=require('../util/path');
+const router=express.Router();
+const productsController=require('../controllers/contacts.js');
+
+router.get('/contactus', productsController.getContacts);
+
+router.post('/contactus',productsController.postContacts);
+router.get('/success',productsController.getSuccess);
+module.exports=router;
